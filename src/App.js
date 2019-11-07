f20589ce0f9c91f8e6c6a0f960c76ed0a2097079
DEV_YUN <riosee2415@gmail.com>
2019-11-07 15:43:55 +0900

    How to send Data AND use Data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({ fav }) {
+  console.log(fav);
+  return <h3>I Like {fav}</h3>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello React Project</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
