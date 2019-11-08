3fe6a832a5bec61d2c91b07ae117b6e0fe62e930
DEV_YUN <riosee2415@gmail.com>
2019-11-08 10:58:09 +0900

    ready that will use fetch movie data at API

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
 import React from "react";
+import { throwStatement } from "@babel/types";
 //import PropTypes from "prop-types";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("constructor");
-  }
-
   state = {
-    count: 0
-  };
-
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
+    isLoading: true,
+    movies: []
   };
 
   componentDidMount() {
-    console.log("component rendered");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 5000);
   }
-
-  componentDidUpdate() {
-    console.log("I just update");
-  }
-
   render() {
-    console.log("Im rendering");
-    return (
-      <div>
-        <h1> The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add </button>
-        <button onClick={this.minus}>Minus </button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We Are Ready"}</div>;
   }
 }
 
