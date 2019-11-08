c0ffc3dc89191fce6956e5e9e611d372cae9f08e
DEV_YUN <riosee2415@gmail.com>
2019-11-08 16:19:11 +0900

    Updates

---
+++ precache-manifest.0a6ed3b9ac88e5b24a71331f902f27af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b246d5ff07bbc3c21601d0a32a3b2fc",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "ec13ba5ec3d2f4a8ef93",
+    "url": "/react-movie-app/static/css/main.7a7a8c95.chunk.css"
+  },
+  {
+    "revision": "adcb127d4d5de1af780e",
+    "url": "/react-movie-app/static/js/2.b8346387.chunk.js"
+  },
+  {
+    "revision": "ec13ba5ec3d2f4a8ef93",
+    "url": "/react-movie-app/static/js/main.e816f3bc.chunk.js"
+  },
+  {
+    "revision": "53b5dcb405b0310d0596",
+    "url": "/react-movie-app/static/js/runtime-main.8162ee61.js"
+  }
+]);
